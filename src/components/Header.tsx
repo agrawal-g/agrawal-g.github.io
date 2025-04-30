40d39a967fe9d44de1604927fec8a0bac9b0323d
Gunwati Agrawal <gunwati.agrawal@icloud.com>
2025-04-29 21:57:28 -0500

    Adjusted header links to account for HashRouter

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,22 @@
-
-//import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 //const Header: React.FC = () -> {
 function Header() {
   return (
     <header className="header">
       <div className="logo"><a className="logo-a" href="/">Gunwati Agrawal</a></div>
-      <nav>
-           <a href="/">Home</a>
-    <a href="/projects">Projects</a>
-    <a href="/tutoring">Tutoring</a>
-    {/*/!*<a href="/blog">Blog</a>*!/ Uncomment to add blog link*/}
+        <nav>
+        <Link to="/">Home</Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="/tutoring">Tutoring</Link>
+        {/* <Link to="/blog">Blog</Link> Uncomment to add blog link */}
       </nav>
+    {/*  <nav>*/}
+    {/*       <a href="/">Home</a>*/}
+    {/*<a href="/projects">Projects</a>*/}
+    {/*<a href="/tutoring">Tutoring</a>*/}
+    {/*/!*<a href="/blog">Blog</a>*!/ */}
+    {/*  </nav>*/}
     </header>
   );
 }
